d1009eecb4cc90779df8f0de2eb2414d921e8e30
parchea <79390380+parchea@users.noreply.github.com>
2021-09-10 12:57:57 -0400

    Update Lab1.java

--- Lab1/Lab1.java
+++ Lab1/Lab1.java
@@ -1,1 +1,38 @@
+import java.util.Scanner;
 
+public class Lab1
+{
+ public static String checking(double speed)
+  {
+    String res="";
+    if (speed > 65.0)
+      {
+        res = "SPEEDING";
+        return res;
+      }
+    else if (speed > 0.0 && speed <= 65.0)
+      {
+        res = "OK";
+        return res;
+      }
+    else if (speed == 0.0)
+      {
+        res = "STOPPED";
+        return res;
+      }
+    else if (speed < 0.0)
+      {
+        res = "BACKWARD";
+        return res;
+      }
+    return  res;
+  }
+    public static void main(String args [])
+    {
+      Scanner kb = new Scanner(System.in);
+      System.out.print("Enter your Speed: ");
+      double fast = kb.nextDouble();
+      String result = checking(fast);
+      System.out.println(result);
+    }
+}
