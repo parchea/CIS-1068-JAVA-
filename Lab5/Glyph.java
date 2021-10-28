d10f51cb48bb4f2bca46f16a9349c5cd986424bf
parchea <79390380+parchea@users.noreply.github.com>
2021-10-28 09:18:56 -0400

    Update Glyph.java
    Fix the number of character are found in this file. 
    Change were in line 79.

--- Lab5/Glyph.java
+++ Lab5/Glyph.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 import java.io.*;
 
@@ -8,8 +7,9 @@ public class Glyph
     private byte ascii;
     private int count;
 
+
     public Glyph(char me_assign, byte ascii_assign, int count_assign)
-    {  
+    {
         this.me = me_assign;
         this.ascii = ascii_assign;
         this.count = count_assign;
@@ -30,75 +30,74 @@ public class Glyph
     {
         this.count++;
     }
-    public static int find(Glyph [] a, char c)
+    public static int find (Glyph [] a, char c)
     {
         for(int i = 0; i < a.length; i++)
         {
-            if(a[i]!=null && a[i].me == c)
+            if(a[i] != null && a[i].me == c)
             {
                 return i;
             }
         }
         return -1;
-    }   
+    }
     public static void sort(Glyph [] a)
     {
-        for(int i = 0; i < a.length && a[i]!=null; i++)
+        for(int i = 0; i < a.length && a[i] != null; i++)
         {
-            for(int j = i+1; j < a.length && a[j]!=null; j++)
+            for(int j = i+1; j < a.length && a[j] != null; j++)
             {
                 if(a[i].count < a[j].count)
                 {
-                    Glyph temp = a [i];
+                    Glyph temp = a[i];
                     a[i] = a[j];
                     a[j] = temp;
                 }
             }
         }
     }
-    public static void main(String [] args)
+    public static void main(String[] args)
     {
-        Glyph [] myArray = new Glyph[512];        
-        Scanner inputstream = null;
+        Glyph[] myArray = new Glyph[512];
         int c1 = 0;
         int helper = 0;
         int index = 0;
-        String filename = "C:\\Users\\Par\\Documents\\CIS 1068\\Lab 5\\test.txt";
-       
+        Scanner inputstream = null;
+        String filename = "E:\\CIS 1068\\Lab 5\\test.txt";
+        
         try
         {
             inputstream = new Scanner(new File(filename));
+
         }
         catch (FileNotFoundException e)
         {
-            System.out.println("The file does not exist.");
+            System.out.println ("File not found: " + filename);
             System.exit(1);
-        }      
-        
-        while(inputstream.hasNext())
+        }
+
+        while(inputstream.hasNextLine())
         {
             String line = inputstream.nextLine();
-
-                for(int i = 0; i < line.length(); i++)
-                    {
-                        char cur = line.charAt(i);
-                        c1++;
-                        byte b = (byte)cur;
-                        int position = find(myArray, cur);
-                        if(position == -1)
-                        {
-                            Glyph F1 = new Glyph(cur,b,1);
-                            myArray[helper] = F1;
-                            helper++;
-                        }
-                        else{
-                            myArray[position].Setcount();
-                        }
-                    }
-            
+            for(int i = 0; i < line.length(); i++)
+            {
+                char cur = line.charAt(i);
+                c1++;
+                byte b = (byte)cur;
+                int position = find(myArray, cur);
+                if (position == -1)
+                {
+                    Glyph F1 = new Glyph(cur,b,1);
+                    myArray[helper] = F1;
+                    helper++;
+                }
+                else
+                {
+                    myArray[position].Setcount();
+                }
+            }
         }
         sort(myArray);
-
         System.out.println("Total of number of character in the file is " + c1);
         System.out.printf( "%-15s %-15s %-15s %n", "Character", "Ascii", "Frequency");
         for (int i = 0; i < myArray.length; i++)
@@ -109,5 +108,4 @@ public class Glyph
             System.out.printf("%-15s %-15d %-15.4f %n",(myArray[i].getMe()), (myArray[i].getAscii()), (k1 / c1));
         }
     }
-
 }
